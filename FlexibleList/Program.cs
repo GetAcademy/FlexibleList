e49543a9d83c424af24cb30c33f3d71465afb0f8
GET Academy <terje@getacademy.no>
2022-01-20 13:53:39 +0100

    Update Program.cs

--- FlexibleList/Program.cs
+++ FlexibleList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FlexibleList
 {
@@ -6,27 +7,86 @@ namespace FlexibleList
     {
         static void Main(string[] args)
         {
-            var list = new FlexibleList<bool>();
+            var lists = new List<List<int>> { new(), new(), new() };
+
             while (true)
             {
+
+                Console.Write("Velg en liste (a, b eller c): ");
+                var listName = Console.ReadLine();
+                var listChar = listName[0];
+                var listIndex = listChar - 'a';
+                var list = lists[listIndex];
+
                 Console.Write("Skriv et tall: ");
                 var numberOrCommand = Console.ReadLine();
                 if (numberOrCommand.StartsWith("-"))
                 {
                     var index = Convert.ToInt32(numberOrCommand.Substring(1));
-                    list.Remove(index);
+                    list.RemoveAt(index);
                 }
                 else
                 {
-                    var number = Convert.ToBoolean(numberOrCommand);
+                    var number = Convert.ToInt32(numberOrCommand);
                     list.Add(number);
                 }
 
-                Console.WriteLine(list.Get());
+                Console.WriteLine("A: " + string.Join(' ', lists[0]));
+                Console.WriteLine("B: " + string.Join(' ', lists[1]));
+                Console.WriteLine("C: " + string.Join(' ', lists[2]));
             }
         }
 
+        // innebygget liste
+        //static void Main(string[] args)
+        //{
+        //    var list = new List<bool>();
+        //    while (true)
+        //    {
+        //        Console.Write("Skriv et tall: ");
+        //        var numberOrCommand = Console.ReadLine();
+        //        if (numberOrCommand.StartsWith("-"))
+        //        {
+        //            var index = Convert.ToInt32(numberOrCommand.Substring(1));
+        //            list.RemoveAt(index);
+        //        }
+        //        else
+        //        {
+        //            var number = Convert.ToBoolean(numberOrCommand);
+        //            list.Add(number);
+        //        }
+
+        //        Console.WriteLine(string.Join(' ', list));
+        //    }
+        //}
+
+
+        //generic-liste
+        //static void Main(string[] args)
+        //{
+        //    var list = new FlexibleList<int>();
+        //    while (true)
+        //    {
+        //        Console.Write("Skriv et tall: ");
+        //        var numberOrCommand = Console.ReadLine();
+        //        if (numberOrCommand.StartsWith("-"))
+        //        {
+        //            var index = Convert.ToInt32(numberOrCommand.Substring(1));
+        //            list.Remove(index);
+        //        }
+        //        else
+        //        {
+        //            var number = Convert.ToInt32(numberOrCommand);
+        //            list.Add(number);
+        //        }
+
+        //        Console.WriteLine(list.Get());
+        //    }
+        //}
+
+
         /*
+         int-liste
         static void Main(string[] args)
         {
             var initialLength = 4;
