d3766b376b5beb0538ae9c4a8523121ba9a951f3
GET Academy <terje@getacademy.no>
2022-01-20 13:04:46 +0100

    Initial commit

---
+++ FlexibleList/FlexibleList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FlexibleList
+{
+    internal class FlexibleList
+    {
+        private int[] _numbers;
+        private int _index;
+
+        public FlexibleList()
+        {
+            _numbers = new int[4];
+            _index = 0;
+        }
+        public void Add(string numberStr)
+        {
+            var number = Convert.ToInt32(numberStr);
+            Add(number);
+        }
+
+        public void Add(int number)
+        {
+            if (_index >= _numbers.Length)
+            {
+                Console.WriteLine($"Dobler kapasitet til {_numbers.Length * 2}");
+                var newNumbers = new int[_numbers.Length * 2];
+                Array.Copy(_numbers, newNumbers, _numbers.Length);
+                _numbers = newNumbers;
+            }
+            _numbers[_index] = number;
+            _index++;
+        }
+
+        public string Get()
+        {
+            return "Tallene er: " + string.Join(',', _numbers);
+        }
+    }
+}
---
+++ FlexibleList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FlexibleList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new FlexibleList();
+            while (true)
+            {
+                Console.Write("Skriv et tall: ");
+                var number = Console.ReadLine();
+                list.Add(number);
+                Console.WriteLine(list.Get());
+            }
+        }
+
+        /*
+        static void Main(string[] args)
+        {
+            var initialLength = 4;
+            var numbers = new int[initialLength];
+            var index = 0;
+            while (true)
+            {
+                Console.Write("Skriv en bokstavtall: ");
+                Console.Write("Skriv et tall: ");
+                var numberStr = Console.ReadLine();
+                var number = Convert.ToInt32(numberStr);
+                if (index >= numbers.Length)
+                {
+                    Console.WriteLine($"Dobler kapasitet til {numbers.Length * 2}");
+                    var newNumbers = new int[numbers.Length * 2];
+                    Array.Copy(numbers, newNumbers, numbers.Length);
+                    numbers = newNumbers;
+                }
+
+                numbers[index] = number;
+                Console.WriteLine("Tallene er: " + string.Join(',', numbers));
+                index++;
+            }
+        }         
+         */
+    }
+}
